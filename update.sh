1d9cc5bfd67b061d663b1634bc5482193b07d7cd
FooTearth <footearth@gmail.com>
2016-03-16 22:34:44 +0800

    use git submodule to import third-party dockerfile

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cwd="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+
+set -e
+
+npm run reset
+
+sed -ie 's/webcenter\/openjdk-jre:8/mooxe\/java8/g' \
+  ./activemq/Dockerfile
+
+rm -rf ./activemq/Dockerfilee
